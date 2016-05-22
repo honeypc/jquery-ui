02a9f60336273c5aa1210dd4caf4907580ab9880
Felix Nagel <info@felixnagel.com>
2016-05-22 22:23:01 +0200

    Calendar tests: Add basic value option tests

--- tests/unit/calendar/options.js
+++ tests/unit/calendar/options.js
@@ -313,6 +313,21 @@ test( "numberOfMonths", function() {
 	);
 } );
 
+test( "value", function( assert ) {
+	expect( 4 );
+
+	var date = new Date( 2016, 5 - 1, 23 );
+
+	assert.equal( element.calendar( "option", "value" ), null, "Initial value" );
+
+	element.calendar( "option", "value", date );
+	assert.dateEqual( element.calendar( "option", "value" ), date, "Value set" );
+	equal( widget.find( "table button.ui-state-active" ).data( "timestamp" ), 1463954400000, "Active button timestamp" );
+
+	element.calendar( "option", "value", "invalid" );
+	assert.dateEqual( element.calendar( "option", "value" ), date, "Value after invalid parameter" );
+} );
+
 /*
 // TODO: Move this to $.date, Globalize or calendar widget
 test( "daylightSaving", function() {
